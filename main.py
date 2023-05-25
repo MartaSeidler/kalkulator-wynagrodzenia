0aecaf083a26e812416fbcd78d0702525878eee0
MartaSeidler <martaseidler2312@gmail.com>
2023-05-25 22:00:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,75 +1,142 @@
 def start():
     print("""
-*** KALKULATOR WYNAGRODZENIA NETTO DO UMOWY O PRACĘ W 2023 ROKU ***
+    *** KALKULATOR WYNAGRODZENIA NETTO DO UMOWY O PRACĘ W 2023 ROKU ***
 """)
 
-def pobranie_wynagrodzenia_brutto():
-    brutto = int(input("Podaj wynagrodzenie brutto: "))
+
+def pobranie_brutto():
+    brutto = int(input("    Podaj wynagrodzenie brutto: "))
     return brutto
 
-def obliczenie_skladki_emerytalnej(brutto):
-    skladka_emerytalna_procent = 9.76 / 100
-    skladka_emerytalna = round((skladka_emerytalna_procent * brutto), 2)
-    return skladka_emerytalna
 
-def obliczenie_skladki_rentowej(brutto):
-    skladka_rentowa_procent = 1.5 / 100
-    skladka_rentowa = round((skladka_rentowa_procent * brutto), 2)
-    return skladka_rentowa
+def obliczenie_skladki(procent, podstawa):
+    return round((procent * podstawa), 2)
 
-def obliczenie_skladki_chorobowej(brutto):
-    skladka_chorobowa_procent = 2.45 / 100
-    skladka_chorobowa = round((skladka_chorobowa_procent * brutto), 2)
-    return skladka_chorobowa
 
-def obliczenie_skladek_razem(skladka_emerytalna, skladka_rentowa, skladka_chorobowa):
-    skladki = skladka_emerytalna + skladka_rentowa + skladka_chorobowa
-    return skladki
+def obliczenie_kosztow_uzyskania_przychodu():
 
-def obliczenie_skladki_zdrowotnej(brutto, skladki):
-    podstawa_skladki_zdrowotnej = brutto - skladki
-    skladka_zdrowotna_procent = 9 / 100
-    skladka_zdrowotna = round((skladka_zdrowotna_procent * podstawa_skladki_zdrowotnej), 2)
-    return skladka_zdrowotna
+    czy_koszty_zwiekszone = input("""
+    Czy Twój zakład pracy znajduje się poza miejscowością, w której mieszkasz?
+    t = tak
+    n = nie
+    """)
+
+    if czy_koszty_zwiekszone == "t":
+        return 300
+    elif czy_koszty_zwiekszone == "n":
+        return 250
+    else:
+        return print("Błąd kosztów uzyskania przychodu")
+
+
+def obliczenie_ulgi_podatkowej():
+    czy_ulga_podatkowa = input("""
+    Czy przysługuje Ci ulga podatkowa?
+    t = tak
+    n = nie
+    """)
+
+    if czy_ulga_podatkowa == "t":
+        return 300
+    elif czy_ulga_podatkowa == "n":
+        return 0
+    else:
+        return print("Błąd ulgi podatkowej")
+
+
+def obliczenie_komornika(koszty_uzyskania_przychodu):
+
+    czy_komornik = input("""
+    Czy masz zajęcie komornicze?
+    t = tak
+    n = nie
+    """)
+
+    if czy_komornik == "t":
+        czy_alimenty = input("""
+    Czy jest to potrącenie alimentacyjne?
+    t = tak
+    n = nie
+    """)
+        if czy_alimenty == "t":
+            komornik_alimenty_procent = 60 / 100
+            return round((komornik_alimenty_procent * netto), 2)
+        else:
+            if koszty_uzyskania_przychodu == 300:
+                kwota_wolna_od_potracen = 2715.48
+            elif koszty_uzyskania_przychodu == 250:
+                kwota_wolna_od_potracen = 2709.48
+            return netto - kwota_wolna_od_potracen
+    elif czy_komornik == "n":
+        return 0
+    else:
+        return print("Błąd potrącenia komorniczego")
+
+
+def obliczenie_ubezpiecznia():
+    czy_ubezpieczenie_grupowe = input("""
+    Czy jesteś ubezpieczony w ubezpieczeniu grupowym u pracodawcy?
+    t = tak
+    n = nie
+    """)
+
+    if czy_ubezpieczenie_grupowe == "t":
+        return float(input("    Podaj kwotę miesięcznej składki (jako separatora dziesiętnego użyj kropki): "))
+    elif czy_ubezpieczenie_grupowe == "n":
+        return 0
+    else:
+        return print("Błąd ubezpieczenia")
+
+
+def obliczenie_raty_pozyczki():
+    czy_pozyczka = input("""
+    Czy masz zaciągniętą pożyczkę u pracodawcy?
+    t = tak
+    n = nie
+    """)
+
+    if czy_pozyczka == "t":
+        return float(input("    Podaj kwotę miesięcznej raty (jako separatora dziesiętnego użyj kropki): "))
+    elif czy_pozyczka == "n":
+        return 0
+    else:
+        return print("Błąd pożyczki")
+
+
+def obliczenie_innych_potracen():
+    czy_inne_potracenia = input("""
+    Czy masz jakieś inne potrącenia u pracodawcy?
+    t = tak
+    n = nie
+    """)
+
+    if czy_inne_potracenia == "t":
+        return float(
+            input("    Podaj kwotę jednomiesięcznego potrącenia (jako separatora dziesiętnego użyj kropki): "))
+    elif czy_inne_potracenia == "n":
+        return 0
+    else:
+        return print("Błąd innych potrąceń")
+
+
+skladka_emerytalna_procent = 9.76 / 100
+skladka_rentowa_procent = 1.5 / 100
+skladka_chorobowa_procent = 2.45 / 100
+skladka_zdrowotna_procent = 9 / 100
 
 start()
-brutto = pobranie_wynagrodzenia_brutto()
-skladka_emerytalna = obliczenie_skladki_emerytalnej(brutto)
-skladka_rentowa = obliczenie_skladki_rentowej(brutto)
-skladka_chorobowa = obliczenie_skladki_chorobowej(brutto)
-skladki = obliczenie_skladek_razem(skladka_emerytalna, skladka_rentowa, skladka_chorobowa)
-skladka_zdrowotna = obliczenie_skladki_zdrowotnej(brutto, skladki)
+brutto = pobranie_brutto()
 
+skladka_emerytalna = obliczenie_skladki(skladka_emerytalna_procent, brutto)
+skladka_rentowa = obliczenie_skladki(skladka_rentowa_procent, brutto)
+skladka_chorobowa = obliczenie_skladki(skladka_chorobowa_procent, brutto)
+skladki = skladka_emerytalna + skladka_rentowa + skladka_chorobowa
+skladka_zdrowotna = obliczenie_skladki(skladka_zdrowotna_procent, (brutto - skladki))
 
-# obliczenie podatku
-czy_koszty_zwiekszone = input("""
-Czy Twój zakład pracy znajduje się poza miejscowością, w której mieszkasz?
-t = tak
-n = nie
-""")
+koszty_uzyskania_przychodu = obliczenie_kosztow_uzyskania_przychodu()
+ulga_podatkowa = obliczenie_ulgi_podatkowej()
 
-if czy_koszty_zwiekszone == "t":
-    koszty_uzyskania_przychodu = 300
-elif czy_koszty_zwiekszone == "n":
-    koszty_uzyskania_przychodu = 250
-else:
-    print("Błąd kosztów uzyskania przychodu")
-
-czy_ulga_podatkowa = input("""
-Czy przysługuje Ci ulga podatkowa?
-t = tak
-n = nie
-""")
-
-if czy_ulga_podatkowa == "t":
-    ulga_podatkowa = 300
-elif czy_ulga_podatkowa == "n":
-    ulga_podatkowa = 0
-else:
-    print("Błąd ulgi podatkowej")
-
-podstawa_skladki_zdrowotnej = brutto - skladki
-dochod = round(podstawa_skladki_zdrowotnej - koszty_uzyskania_przychodu)
+dochod = round(brutto - skladki - koszty_uzyskania_przychodu)
 procent_podatku = 12 / 100
 podatek = round((procent_podatku * dochod), 2)
 zaliczka_na_podatek = round(podatek - ulga_podatkowa)
@@ -78,112 +145,41 @@ zaliczka_na_podatek = round(podatek - ulga_podatkowa)
 # obliczenie kwoty netto
 netto = round((brutto - skladki - skladka_zdrowotna - zaliczka_na_podatek), 2)
 
+# obliczenie potrąceń
+komornik = obliczenie_komornika(koszty_uzyskania_przychodu)
+ubezpieczenie = obliczenie_ubezpiecznia()
+rata_pozyczki = obliczenie_raty_pozyczki()
+inne_potracenia = obliczenie_innych_potracen()
+
+potracenia = round((komornik + ubezpieczenie + rata_pozyczki + inne_potracenia), 2)
+przelew = round((netto - potracenia), 2)
 
 # wydruk wyników
 print(f"""
-wynagrodzenie brutto: {brutto}
+            * * *
+    
+    wynagrodzenie brutto: {brutto}
 
-składka emerytalna: {skladka_emerytalna},
-składka rentowa: {skladka_rentowa},
-składka chorobowa: {skladka_chorobowa},
-razem składki ZUS: {skladki}
+    składka emerytalna: {skladka_emerytalna}
+    składka rentowa: {skladka_rentowa}
+    składka chorobowa: {skladka_chorobowa}
+    razem składki ZUS: {skladki}
 
-podstawa składki zdrowotnej: {podstawa_skladki_zdrowotnej}
-składka zdrowotna: {skladka_zdrowotna}
+    podstawa składki zdrowotnej: {brutto - skladki}
+    składka zdrowotna: {skladka_zdrowotna}
 
-koszty uzyskania przychodu: {koszty_uzyskania_przychodu}
-dochód: {dochod}
-obliczony podatek: {podatek}
-miesięczna ulga podatkowa: {ulga_podatkowa}
-zaliczka na podatek: {zaliczka_na_podatek}
+    koszty uzyskania przychodu: {koszty_uzyskania_przychodu}
+    dochód: {dochod}
+    obliczony podatek: {podatek}
+    miesięczna ulga podatkowa: {ulga_podatkowa}
+    zaliczka na podatek: {zaliczka_na_podatek}
 
-kwota netto: {netto}""")
+    kwota netto: {netto}
+      
+    potrącenia razem: {potracenia}
+    kwota przelewu na konto: {przelew}
 
-
-# obliczenie zajęcia komorniczego
-czy_komornik = input("""
-Czy masz zajęcie komornicze?
-t = tak
-n = nie
-""")
-
-komornik_alimenty_procent = 60 / 100
-
-if czy_koszty_zwiekszone == "t":
-    kwota_wolna_od_potracen = 2715.48
-elif czy_koszty_zwiekszone == "n":
-    kwota_wolna_od_potracen = 2709.48
-
-if czy_komornik == "t":
-    czy_alimenty = input("""
-Czy jest to potrącenie alimentacyjne?
-t = tak
-n = nie
-""")
-    if czy_alimenty == "t":
-        komornik = round((komornik_alimenty_procent * netto), 2)
-    else:
-        komornik = netto - kwota_wolna_od_potracen
-elif czy_komornik == "n":
-    komornik = 0
-else:
-    print("Błąd potrącenia komorniczego")
-
-
-# obliczenie ubezpiecznia
-czy_ubezpieczenie_grupowe = input("""
-Czy jesteś ubezpieczony w ubezpieczeniu grupowym u pracodawcy?
-t = tak
-n = nie
-""")
-
-if czy_ubezpieczenie_grupowe == "t":
-    ubezpieczenie = float(input("Podaj kwotę miesięcznej składki (jako separatora dziesiętnego użyj kropki): "))
-elif czy_ubezpieczenie_grupowe == "n":
-    ubezpieczenie = 0
-else:
-    print("Błąd ubezpieczenia")
-
-
-# obliczenie raty pożyczki
-czy_pozyczka = input("""
-Czy masz zaciągniętą pożyczkę u pracodawcy?
-t = tak
-n = nie
-""")
-
-if czy_pozyczka == "t":
-    rata_pozyczki = float(input("Podaj kwotę miesięcznej raty (jako separatora dziesiętnego użyj kropki): "))
-elif czy_pozyczka == "n":
-    rata_pozyczki = 0
-else:
-    print("Błąd pożyczki")
-
-
-# obliczenie innych potrąceń
-czy_inne_potracenia = input("""
-Czy masz jakieś inne potrącenia u pracodawcy?
-t = tak
-n = nie
-""")
-
-if czy_inne_potracenia == "t":
-    inne_potracenia = float(
-        input("Podaj kwotę jednomiesięcznego potrącenia (jako separatora dziesiętnego użyj kropki): "))
-elif czy_inne_potracenia == "n":
-    inne_potracenia = 0
-else:
-    print("Błąd innych potrąceń")
-
-
-# obliczenie potrąceń
-potracenia = round((ubezpieczenie + komornik + rata_pozyczki + inne_potracenia), 2)
-przelew = round((netto - potracenia), 2)
-
-print(f"""
-netto: {netto}
-potrącenia razem: {potracenia}
-kwota przelewu na konto: {przelew}
+            * * *
 """)
 
 # wyjście z programu
